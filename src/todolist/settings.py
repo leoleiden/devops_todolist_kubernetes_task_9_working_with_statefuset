23821520e1eaa1d59c66d74c6e5dc3291777567b
Leonid Lachmann <148564629+leoleiden@users.noreply.github.com>
2025-06-27 23:37:07 +0300

    Update settings.py

--- src/todolist/settings.py
+++ src/todolist/settings.py
@@ -1,14 +1,28 @@
 import os
+from django.core.exceptions import ImproperlyConfigured # <-- Додано цей імпорт
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
-DEBUG = True
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
-ALLOWED_HOSTS = ["*"]
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get("SECRET_KEY")
+if not SECRET_KEY:
+    raise ImproperlyConfigured("The SECRET_KEY environment variable must be set.")
 
-DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
+# SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG тепер читається зі змінних оточення і перетворюється на булеве значення
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+
+# Залежно від DEBUG, ALLOWED_HOSTS може бути "*" для розробки або конкретними доменами для продакшену
+ALLOWED_HOSTS = ["*"] if DEBUG else ["your_domain.com", "localhost", "127.0.0.1"] # Оновіть для продакшену
+
+
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField" # Рекомендовано для Django 3.2+
+
+# Application definition
 
 INSTALLED_APPS = (
     "django.contrib.admin",
@@ -17,7 +31,7 @@ INSTALLED_APPS = (
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "todolist",
+    "todolist", # Можливо, це має бути назва вашого кореневого додатку, або це ім'я проекту.
     "lists",
     "accounts",
     "rest_framework",
@@ -25,7 +39,6 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE = (
-    "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -36,18 +49,42 @@ MIDDLEWARE = (
 
 ROOT_URLCONF = "todolist.urls"
 
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
 WSGI_APPLICATION = "todolist.wsgi.application"
+
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "HOST": os.environ.get("MYSQL_HOST"),
-        "PORT": os.environ.get("MYSQL_PORT", "3306"),
+        "PORT": os.environ.get("MYSQL_PORT", "3306"), # Забезпечуємо дефолтне значення
         "NAME": os.environ.get("MYSQL_DATABASE_NAME"),
         "USER": os.environ.get("MYSQL_USER"),
         "PASSWORD": os.environ.get("MYSQL_PASSWORD"),
     }
 }
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
 
@@ -59,29 +96,15 @@ USE_L10N = True
 
 USE_TZ = True
 
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # Додано для збору статичних файлів у продакшені
+
+# Login settings
 
 LOGIN_URL = "/auth/login/"
 
 LOGOUT_URL = "/auth/logout/"
-
-REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
-    ]
-}
-
-TEMPLATES = [
-    {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
-                "django.template.context_processors.request",
-            ],
-            "debug": True,
-        },
-    },
-]
